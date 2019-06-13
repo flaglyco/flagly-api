342d98f7b07de0b6c50c95ff306a5172db1e2d9d
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2019-06-13 15:37:14 +0300

    Update circe encoder and decoder

--- circe/src/main/scala/com/github/makiftutuncu/switchboard/circe/package.scala
+++ circe/src/main/scala/com/github/makiftutuncu/switchboard/circe/package.scala
@@ -2,30 +2,33 @@ package com.github.makiftutuncu.switchboard
 
 import java.util.UUID
 
-import io.circe.{Json, Decoder => CirceDecoder, Encoder => CirceEncoder}
+import io.circe.{
+  Decoder => CirceDecoder,
+  Encoder => CirceEncoder,
+  parser  => CirceParser
+}
 
 package object circe {
-  type JsonDecoder[O] = Decoder[Json, O]
-
-  type JsonEncoder[I] = Encoder[I, Json]
-
-  implicit def jsonDecoder[O](implicit decoder: CirceDecoder[O]): JsonDecoder[O] = (input: Json) => decoder.decodeJson(input).toOption
-
-  implicit def jsonEncoder[I](implicit encoder: CirceEncoder[I]): JsonEncoder[I] = (input: I) => encoder(input)
-
-  implicit def flagDecoder[V](implicit decoder: CirceDecoder[V]): JsonDecoder[Flag[V]] = { input: Json =>
-    val circeDecoder = CirceDecoder.forProduct5("id", "name", "description", "value", "defaultValue") { (id: UUID, name: String, description: String, value: V, defaultValue: V) =>
-      Flag[V](id, name, description, value, defaultValue)
+  implicit def circeJsonDecoder[A](implicit circeDecoder: CirceDecoder[A]): Decoder[A] = { input: String =>
+    for {
+      json <- CirceParser.parse(input).toOption
+      a    <- json.as[A].toOption
+    } yield {
+      a
     }
-
-    circeDecoder.decodeJson(input).toOption
   }
 
-  implicit def flagEncoder[V](implicit encoder: CirceEncoder[V]): JsonEncoder[Flag[V]] = { input: Flag[V] =>
-    val circeEncoder = CirceEncoder.forProduct5("id", "name", "description", "value", "defaultValue") { flag: Flag[V] =>
+  implicit def circeJsonEncoder[A](implicit circeEncoder: CirceEncoder[A]): Encoder[A] = { input: A =>
+    circeEncoder(input).noSpaces
+  }
+
+  implicit def flagCirceDecoder[A](implicit circeDecoder: CirceDecoder[A]): CirceDecoder[Flag[A]] =
+    CirceDecoder.forProduct5("id", "name", "description", "value", "defaultValue") { (id: UUID, name: String, description: String, value: A, defaultValue: A) =>
+      Flag[A](id, name, description, value, defaultValue)
+    }
+
+  implicit def flagCirceEncoder[A](implicit circeEncoder: CirceEncoder[A]): CirceEncoder[Flag[A]] =
+    CirceEncoder.forProduct5("id", "name", "description", "value", "defaultValue") { flag: Flag[A] =>
       (flag.id, flag.name, flag.description, flag.value, flag.defaultValue)
     }
-
-    circeEncoder.apply(input)
-  }
 }
